8f7ba07d4d6b85560f5509ad6f2297f8fa78456f
joshua rowan <47506412+jdrowan87@users.noreply.github.com>
2020-08-29 17:17:59 -0500

    new

---
+++ JSstart/js/script.js
@@ -0,0 +1,10 @@
+var array = new Array();
+array[0] = "herp derp";
+array[1] = 4;
+array[2] = "sherpa";
+array[3] = function (name) {
+	console.log("hello" + name);
+};
+array[4] = {herp: 72};
+
+console.log(array);
